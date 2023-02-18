19d1894f1493184ec96c6f3335d6de0681e31493
park songhee <songhee6392@gmail.com>
2023-02-19 01:11:47 +0900

    수리검을 화면에 추가하기_수리검의 초기 위치는 히어로의 위치와 높이 너비를 기준으로 생성

--- js/class.js
+++ js/class.js
@@ -24,6 +24,7 @@ class Hero {
         //x 눌러서 공격 클래스 추가
         if (key.keyDown["attack"]) {
             this.el.classList.add("attack");
+            new Bullet(); //공격키를 눌렀을 때 수리검 클래스 생성
         }
 
         //사용자가 모두 키를 뗀 경우 run 클래스 삭제해서 대기모드로 변경
@@ -49,7 +50,41 @@ class Hero {
             left: this.el.getBoundingClientRect().left,
             right: this.el.getBoundingClientRect().right,
             top: gameProp.screenHeight - this.el.getBoundingClientRect().top, //화면 bottom을 기준으로 한 히어로의 머리 위치
-            bottom: this.el.getBoundingClientRect().top - gameProp.screenHeight, //아래를 기준으로한 바텀 위치
+            bottom:
+                gameProp.screenHeight -
+                this.el.getBoundingClientRect().top -
+                this.el.getBoundingClientRect().height, //아래를 기준으로한 바텀 위치
         };
+    }
+
+    /**
+     * size() : 히어로 엘리먼트의 너비, 높이 구하기
+     * getBoundingClientRect()를 이용해서 구할 수도 있지만 offset을 이용해서 구한다.
+     */
+    size() {
+        return {
+            width: this.el.offsetWidth,
+            height: this.el.offsetHeight,
+        };
+    }
+}
+
+class Bullet {
+    constructor() {
+        this.parentNode = document.querySelector(".game"); //수리검 엘리먼트를 담을 엘리먼트
+        this.el = document.createElement("div"); //수리검 엘리먼트
+        this.el.className = "hero_bullet";
+        this.x = 0;
+        this.y = 0;
+        this.init();
+    }
+
+    init() {
+        this.x = hero.position().left + hero.size().width / 2; //수리검 위치는 히어로의 위치를 기준으로 담아준다
+        this.y = hero.position().bottom - hero.size().height / 2;
+
+        //수리검의 위치
+        this.el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+        this.parentNode.appendChild(this.el); //수리검 game엘리먼트에 추가
     }
 }
