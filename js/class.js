93339a7f842626de663a00d30a395f9da6600230
park songhee <songhee6392@gmail.com>
2023-02-18 23:33:38 +0900

    캐릭터가 이동하도록 만들기_left, right에 따라서 캐릭터 위치 변경, 키 눌렸을 때 자연스럽게 움직이기 위해서 requestAnimationFrame() 재귀 호출

--- js/class.js
+++ js/class.js
@@ -1,8 +1,8 @@
 class Hero {
     constructor(el) {
         this.el = document.querySelector(el);
-
-        console.log(this.el);
+        this.moveX = 0; //히어로가 이동할 거리
+        this.speed = 16; //히어로의 스피드
     }
 
     /**
@@ -12,9 +12,13 @@ class Hero {
         if (key.keyDown["left"]) {
             this.el.classList.add("run");
             this.el.classList.add("flip"); //왼쪽 방향 눌렀을때 <- 방향 이동
+
+            this.moveX = this.moveX - this.speed; //<- 왼쪽으로 이동해야하기 때문에 마이너스 값(스피드 값 빼면서 감소)
         } else if (key.keyDown["right"]) {
             this.el.classList.add("run");
             this.el.classList.remove("flip"); //flip 클래스 삭제해서 왼쪽 오른쪽 삭제
+
+            this.moveX = this.moveX + this.speed; //-> 오른쪽으로 이동해야 하기 때문에 플러스 값(스피드 값 더하면서 증가)
         }
 
         //x 눌러서 공격 클래스 추가
@@ -31,5 +35,8 @@ class Hero {
         if (!key.keyDown["attack"]) {
             this.el.classList.remove("attack");
         }
+
+        //hero 엘리먼트의 부모인 hero_box에 translateX를 이용해서 hero_box의 위치를 변경시킨다.
+        this.el.parentNode.style.transform = `translateX(${this.moveX}px)`;
     }
 }
--- js/game.js
+++ js/game.js
@@ -9,17 +9,30 @@ const key = {
     },
 };
 
+/**
+ * renderGame() : 키가 눌렸을 떄 키 딜레이를 줄야서 캐릭터가 자연스럽게 움직 일 수 있게 하는 함수
+ * 히어로 캐릭터의 움직임이 자연스럽지 못하다
+ * 원인은 key 이벤트가 연속적으로 발생했을 때 키눌림 이벤트의 딜레이 차이가 있기 때문
+ * 재귀호출을 이용하여 수행
+ * 초당 약 60 프레임을 그리며 renderGame 함수가 무한 반복되면서 keyMotion 함수를 계속적으로 수행한다.
+ * 키 값의 상태를 계속 체크하고 히어로의 움직임에 필요한 값을 변경하면서 키 눌림의 딜레이 없이 부드럽게 움직이게 한다.
+ *
+ * 처음 페이지가 실행되었을 때 호출
+ *  */
+const renderGame = () => {
+    hero.keyMotion(); //키가 눌렸을 때 발생하는 이벤트는 renderGame함수에서 계속 호출
+    window.requestAnimationFrame(renderGame);
+};
+
 const windowEvent = () => {
     //키 누를 때 이벤트
     window.addEventListener("keydown", (event) => {
         key.keyDown[key.keyValue[event.key]] = true;
-        hero.keyMotion();
     });
 
     //키 떼었을 때 발생하는 이벤트
     window.addEventListener("keyup", (event) => {
         key.keyDown[key.keyValue[event.key]] = false;
-        hero.keyMotion();
     });
 };
 
@@ -52,6 +65,7 @@ const init = () => {
     hero = new Hero(".hero"); //클래스 이름 넘기기
     loadImg(); //css에서 사용하는 이미지를 미리 로드시키기 위한 함수
     windowEvent();
+    renderGame(); //키가 눌렸을 떄 키 딜레이를 줄야서 캐릭터가 자연스럽게 움직 일 수 있게 하는 함수
 };
 
 window.onload = () => {
